d0bd96a773dfd402b29eab8471e81203255d723c
xieby1 <xieby1@outlook.com>
2026-03-26 23:37:46 +0800

    add check

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
 .SECONDARY:
 
 MAIN_TYPs = $(shell find . -name "*main.typ")
-PDFs = $(subst .typ,.pdf,${MAIN_TYPs})
+MAIN_PDFs = $(subst .typ,.pdf,${MAIN_TYPs})
+MAIN_CHKs = $(subst .typ,.chk,${MAIN_TYPs})
 
-all: ${PDFs}
+all: ${MAIN_CHKs}
 
 #                    find all typ in the same folder
 %.pdf: %.typ $(shell find $(<D) -name "*.typ") lib.typ
 	typst compile $< $@
 
+%main.chk: %main.pdf %main.regex
+	pdftotext $< - | check_regex_order.py $(filter-out $<,$^)
+
 clean:
-	rm -f ${PDFs}
+	rm -f ${MAIN_PDFs}
--- default.nix
+++ default.nix
@@ -8,6 +8,19 @@ let
       hash = "sha256-sF4dViV0o4Mgupl/akmKaWdr5qV8KToPm46II1xk8C8=";
     }
   ) {};
+  check_regex_order-py = pkgs.runCommand "check_regex_order.py" {} ''
+    mkdir -p $out/bin/
+    ln -s
+  '';
+  miao = pkgs.writeScriptBin;
 in local-typst-env {
   src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+  nativeBuildInputs = [
+    # for tests
+    pkgs.poppler-utils
+    (let src = pkgs.fetchurl {
+      url = "https://github.com/xieby1/nestoc/raw/b893af13e082caae5073c16605625f4cb86e6634/doc/check_regex_order.py";
+      sha256 = "1kf7p3yba100i3ir7zs2cgpivrr2369mkckcpas5n8z64pxgf2jh";
+    }; in pkgs.writeScriptBin src.name "${pkgs.python3}/bin/python3 ${src} $@")
+  ];
 }
