09f317ac4c231d382b6d5369b0d5aac057b1a830
xieby1 <xieby1@outlook.com>
2026-07-29 11:22:31 +0800

    chore: use npins

--- default.nix
+++ default.nix
@@ -1,13 +1,7 @@
 {
-  pkgs ? import <nixpkgs> {},
-  local-typst-env ? pkgs.callPackage (
-    pkgs.fetchFromGitHub {
-      owner = "xieby1";
-      repo = "local-typst-env";
-      rev = "a914fcd702e3c0097acc9974100ab2e9a2d968a9";
-      hash = "sha256-sF4dViV0o4Mgupl/akmKaWdr5qV8KToPm46II1xk8C8=";
-    }
-  ) {},
+  npinsed ? import ./npins,
+  pkgs ? import npinsed.nixpkgs {},
+  local-typst-env ? pkgs.callPackage npinsed.local-typst-env.outPath {},
 }: local-typst-env {
   src = pkgs.nix-gitignore.gitignoreSource [] ./.;
   nativeBuildInputs = [
