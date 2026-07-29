9f36c01160222d78f3621295dd74e6b55f729e78
xieby1 <xieby1@outlook.com>
2026-07-29 11:14:58 +0800

    chore: parameterization

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
-let
-  pkgs = import <nixpkgs> {};
-  local-typst-env = pkgs.callPackage (
+{
+  pkgs ? import <nixpkgs> {},
+  local-typst-env ? pkgs.callPackage (
     pkgs.fetchFromGitHub {
       owner = "xieby1";
       repo = "local-typst-env";
       rev = "a914fcd702e3c0097acc9974100ab2e9a2d968a9";
       hash = "sha256-sF4dViV0o4Mgupl/akmKaWdr5qV8KToPm46II1xk8C8=";
     }
-  ) {};
-in local-typst-env {
+  ) {},
+}: local-typst-env {
   src = pkgs.nix-gitignore.gitignoreSource [] ./.;
   nativeBuildInputs = [
     # for tests
